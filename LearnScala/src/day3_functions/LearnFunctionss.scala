bca082e03a7cc118c876f28b2f3e4b1c9eba00e7
Prateek <prateeksethiDev@gmail.com>
2020-11-10 12:37:16 +0530

    adding another method to higher order function call

--- LearnScala/src/day3_functions/LearnFunctionss.scala
+++ LearnScala/src/day3_functions/LearnFunctionss.scala
@@ -57,6 +57,7 @@ object LearnFunctionss {
   println("-----------Processing the higher order functions-----------------")
   println(highOrderFun(9,dobleTheNumber))
   
+  
   println("-----------Processing the higher order functions to triple the number-----------------")
   val tripleTheNumber=(num:Int)=>num*3
   println(highOrderFun(9,tripleTheNumber))
